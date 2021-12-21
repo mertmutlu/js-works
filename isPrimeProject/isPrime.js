f4cc49f9967a1a8aea7131e6c067e2cff2736b79
mertmutlu <mertmutlu33@gmail.com>
2021-12-21 12:09:36 +0300

    init()

--- isPrimeProject/isPrime.js
+++ isPrimeProject/isPrime.js
@@ -1,10 +1,47 @@
 console.log("Started");
- 
+/*
 function myFunction(){
     console.log("Clicked");
-     var myvalue = document.getElementById('name');
+     var myvalue = document.getElementsByName('txtname').value;
      console.log(myvalue);
     return myvalue;
     
 }
+*/
+var myindex=0;
+function myFunction(){
+    
+    var items = [];
+    // Selecting the input element and get its value 
+    var inputVal = document.getElementById("txtname").value;
+    
+    items.push(inputVal);  
+    myindex++;
 
+    console.log(items,myindex);
+    
+    var x = document.createElement("div");  // Creates a new <div> node
+    x.textContent = inputVal;         // Sets the text content
+    document.body.appendChild(x);  
+    
+    var person = document.createElement("div");  // Creates a new <div> node
+    person.textContent = myindex;         // Sets the text content
+    document.body.appendChild(person);  
+
+   // document.getElementById("personNumber").innerText=person;
+}
+
+function startShake(){
+    console.log(toString(x));
+}
+
+/*
+input.addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      document.getElementById("ekle").click();
+     }
+  });
+  */
